8941bafb099ee98534c1188113f5b270c489b9ae
Martín <mart.salasm@duocuc.cl>
2021-05-06 14:46:31 -0400

    added the game and its results

--- script.js
+++ script.js
@@ -11,4 +11,36 @@ function computerPlay(){
     const random = Math.floor(Math.random()*rockpaperscissors.length);
     return rockpaperscissors[random]
 }
-console.log(computerPlay())
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === rock && computerSelection===paper){
+    alert("You lose! Computer chose paper and paper beats rock")
+    return false
+    }
+    else if (playerSelection===rock && computerSelection===scissors){
+    alert("You win! computer chose scissors and rock beats scissors!")
+    return true
+    }
+    else if(playerSelection===paper && computerSelection===scissors){
+    alert("You lose! Computer chose scissors and scissors beats paper!")
+    return false
+    }
+    else if (playerSelection===paper && computerSelection===rock){
+    alert("You win! Computer chose rock and paper beats rock!")
+    return true
+    }
+    else if (playerSelection===scissors && computerSelection===rock){
+    alert("You lose! computer chose rock and rock beats scissors!")
+    return false
+    }
+    else if(playerSelection===scissors && computerSelection===paper){
+    alert("You win! Computer chose paper and scissors beats paper!")
+    return true
+    }
+    else{
+    return alert("It's a draw! both players chose the same!")
+    }
+}
+var playerSelection=prompt()
+playerSelection=playerSelection.toLowerCase()
+const computerSelection=computerPlay();
+console.log(playRound(playerSelection,computerSelection))
