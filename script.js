c114a64a48cab89f428dd0f94eef0f93267de824
Martín <mart.salasm@duocuc.cl>
2021-05-06 14:09:17 -0400

    added the computer's random response

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello world")
+// Declared the variables for rock paper and scissors
+const rock="rock";
+const paper="paper";
+const scissors="scissors";
+//made a list that includes the playing options.
+const rockpaperscissors= [rock,paper,scissors];
+
+function computerPlay(){
+//found a way to make the computer choose a random option. 
+    const random = Math.floor(Math.random()*rockpaperscissors.length);
+    return rockpaperscissors[random]
+}
+console.log(computerPlay())
